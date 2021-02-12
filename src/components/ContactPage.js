360dcd09740466525def62e9a492d5d59fbc5bdd
Amrit Singh <amrit.sbagga@gmail.com>
2021-02-13 01:07:32 +0530

    initial boilerplate with few portfolio components

---
+++ src/components/ContactPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ContactPage = () => (
+    <div>
+        <h1>Contact me</h1>
+        <p>You can reach me at amrit@xyzmail.com</p>
+    </div>
+);
+
+export default ContactPage;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+//entry -> output
+
+const path = require('path')
+//console.log(path.join(__dirname, 'public'));
+
+module.exports = {
+    entry : './src/app.js',
+    output : {
+        path: path.join(__dirname, 'public'),
+        filename : 'bundle.js'
+    },
+    module: {
+        rules : [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test : /\.s?css$/,
+                use :[
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    devtool : 'cheap-module-source-map',
+    mode:'development',
+    devServer : {
+        contentBase : path.join(__dirname, 'public'),
+        historyApiFallback : true
+    }
+};
+
+//check webpack docs available at https://webpack.js.org/configuration 
+//loader
+//webpack.js.org/configuration/dev-server
